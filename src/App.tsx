b715068917201f5468fd236127f0f4ba1d7c2534
ICarlota99 <k431999@gmail.com>
2025-06-25 04:29:39 -0400

    initial setup with vite+react

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { ScrollProgress } from "@/components/ScrollProgress";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutMeSection from "@/components/sections/AboutMeSection";
+// import CareerGoalsSection from "@/components/sections/CareerGoalsSection";
+import ProjectsSection from "@/components/sections/ProjectsSection";
+import TechStackSection from "@/components/sections/TechStackSection";
+import ResumeSection from "@/components/sections/ResumeSection";
+import ContactSection from "@/components/sections/ContactSection";
+
+export default function HomePage() {
+  return (
+    <main className="bg-background text-foreground">
+      <ScrollProgress />
+      <HeroSection />
+      <AboutMeSection />
+      {/* <CareerGoalsSection /> */}
+      <ProjectsSection />
+      <TechStackSection />
+      <ResumeSection />
+      <ContactSection />
+    </main>
+  );
+}
