41b833f049d1676e96ee6355c962db564c66fd1c
ICarlota99 <k431999@gmail.com>
2025-07-03 19:08:14 -0400

    add sections, navbar and scroll progress bar to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { ScrollProgress } from "@/components/ScrollProgress";
+import { ScrollProgress } from "@/components/ui/ScrollProgress";
+import Navbar from "@/components/layout/Navbar";
 import HeroSection from "@/components/sections/HeroSection";
-import AboutMeSection from "@/components/sections/AboutMeSection";
+import AboutSection from "@/components/sections/AboutSection";
 // import CareerGoalsSection from "@/components/sections/CareerGoalsSection";
-import ProjectsSection from "@/components/sections/ProjectsSection";
-import TechStackSection from "@/components/sections/TechStackSection";
-import ResumeSection from "@/components/sections/ResumeSection";
-import ContactSection from "@/components/sections/ContactSection";
+// import ProjectsSection from "@/components/sections/ProjectsSection";
+// import TechStackSection from "@/components/sections/TechStackSection";
+// import ResumeSection from "@/components/sections/ResumeSection";
+// import ContactSection from "@/components/sections/ContactSection";
 
 export default function HomePage() {
   return (
     <main className="bg-background text-foreground">
       <ScrollProgress />
+      <Navbar />
       <HeroSection />
-      <AboutMeSection />
+      <AboutSection />
       {/* <CareerGoalsSection /> */}
-      <ProjectsSection />
-      <TechStackSection />
-      <ResumeSection />
-      <ContactSection />
+      {/* <ProjectsSection /> */}
+      {/* <TechStackSection /> */}
+      {/* <ResumeSection /> */}
+      {/* <ContactSection /> */}
     </main>
   );
 }
