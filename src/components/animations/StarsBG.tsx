32699ebfeba4a8be4951207e68bde014fb59779e
ICarlota99 <k431999@gmail.com>
2025-07-07 15:17:59 -0400

    perf: delete lazy loading and reduce stars count and factor to check performance (#1 improved performance from 40 to 52)

--- src/components/animations/StarsBG.tsx
+++ src/components/animations/StarsBG.tsx
@@ -1,22 +1,12 @@
-import { lazy, Suspense } from "react";
-
-const Canvas = lazy(() => import("@react-three/fiber").then(mod => ({ default: mod.Canvas })));
-const Stars = lazy(() => import("@react-three/drei").then(mod => ({ default: mod.Stars })));
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
 
 export default function StarsBG() {
-  return (
-    <div className="fixed inset-0 -z-10">
-      <Suspense fallback={null}>
-        <Canvas
-          dpr={[1, 1.5]}
-          frameloop="demand"
-          camera={{ position: [0, 0, 1], far: 100, near: 1 }}
-        >
-          <Suspense fallback={null}>
-            <Stars radius={30} count={1000} factor={3} fade speed={1} />
-          </Suspense>
-        </Canvas>
-      </Suspense>
-    </div>
-  );
-}
+    return(
+        <div className="fixed inset-0 -z-10">
+            <Canvas>
+                <Stars radius={50} count={400} factor={3} fade speed={2} />
+            </Canvas>
+        </div>
+    );
+};
