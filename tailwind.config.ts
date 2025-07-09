58eb70dd65e570a07caf3fe45287c69687a7537a
ICarlota99 <k431999@gmail.com>
2025-07-09 15:12:29 -0400

    fix typos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  darkMode: [ "class" ],
+  // darkMode: [ "class" ],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -122,7 +122,7 @@ const config: Config = {
       }
     }
   },
-  plugins: [ tailwindcssAnimate, require("tailwindcss-animate") ],
+  plugins: [ tailwindcssAnimate],
 };
 
 export default config;
