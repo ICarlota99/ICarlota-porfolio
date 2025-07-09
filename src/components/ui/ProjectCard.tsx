edc7b6be098190ee9d670bc9d2677b28b3071e28
ICarlota99 <k431999@gmail.com>
2025-07-09 15:14:43 -0400

    style: fix size of github icon

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -107,7 +107,7 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
                 onClick={ handleButtonClick }
                 className="flex-1 text-center sm:text-left inline-flex items-center justify-center sm:justify-start gap-2 bg-gray-700 hover:bg-gray-600 text-gray-300 hover:text-white font-medium py-2 px-4 rounded-lg text-sm transform active:scale-95 transition-all duration-150 ease-in-out"
               >
-                <GithubIcon size={ 16 } />
+                <GithubIcon className="w-16 h-16" />
                 View Code
               </a>
             ) }
